d5e318423a5e15680c26469a30a36f123eed0800
Qylwok <raphael.lapertot@edu.esiee.fr>
2018-11-10 14:49:02 +0100

    Réseau simple (1 neurone) en tensorflow

--- 03_debut_tensorflow.py
+++ 03_debut_tensorflow.py
@@ -1,6 +1,15 @@
+"""
+On va maintenant utiliser Tensorflow
+Tensorflow utilise un système de graph, dans lequel on va définir des opérations,
+puis exécuter ces opérations.
+Le graph a besoin qu'on lui indique ses entrées (==> placeholder) et ses sorties
+"""
+
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 def get_dataset():
     """
@@ -25,5 +34,51 @@ if __name__ == '__main__':
     # Dataset
     features, targets = get_dataset()
     # Affichage des points
-    plt.scatter(features[:, 0], features[:, 1], s=40, c=targets, cmap=plt.cm.Spectral)
-    plt.show()
+    # plt.scatter(features[:, 0], features[:, 1], s=40, c=targets, cmap=plt.cm.Spectral)
+    # plt.show()
+
+    # Un placeholder indique au graph les entrées dont il va avoir besoin. 
+    # - None peut etre remplacé par 400
+    tf_features = tf.placeholder(tf.float32, shape=[None, 2])
+    tf_targets = tf.placeholder(tf.float32, shape=[None, 1])
+
+    # Impression des features dans le graph de la session
+    #print(sess.run(tf_features, feed_dict={
+    #    tf_features: features
+    #}))
+
+    # Déclaration puis initialisation des variables
+    w = tf.Variable(tf.random_normal([2, 1]))
+    b = tf.Variable(tf.zeros([1]))
+
+    # Déclaration de la préactivation puis de l'activation
+    z = tf.matmul(tf_features, w) + b
+    py = tf.nn.sigmoid(z)
+
+    # Calcul du cout (erreur) de la prédiction par rapport à la valeur réelle
+    cost = tf.reduce_mean(tf.square(py - tf_targets))
+
+    # Détermination de la précision de la prédiction effectuée
+    correct_prediction = tf.equal(tf.round(py), tf_targets)
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    # Optimisation des poids et biais en utilisant une descente de gradient
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+    train = optimizer.minimize(cost)
+
+
+    # Une session contient le graph
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
+
+    for e in range(1000):
+        sess.run(train, feed_dict={
+            tf_features: features,
+            tf_targets: targets
+        })
+
+        if e % 50 == 0:
+            print("accuracy = ", sess.run(accuracy, feed_dict={
+                tf_features: features,
+                tf_targets: targets
+            }))
